c50142587776bb3619c325dea733c26a91b24d71
skeptycal <skeptycal@gmail.com>
2020-04-06 09:11:20 -0500

    install deps

--- setup.py
+++ setup.py
@@ -3,23 +3,24 @@
 
 from __future__ import absolute_import
 
-if True: # stdlib imports
+if True:  # stdlib imports
     import os
     import pprint
     import sys
     from pathlib import Path
-    import ast                      # safer eval ...
-    from os import linesep as NL    # platform specific newline
-    from os import sep as PATHSEP   # platform specific path separator
-    from typing import Dict, List            # >= 3.6 type hints support
+    import ast  # safer eval ...
+    from os import linesep as NL  # platform specific newline
+    from os import sep as PATHSEP  # platform specific path separator
+    from typing import Dict, List  # >= 3.6 type hints support
 
-if True: # setup tools
+if True:  # setup tools
     from setuptools import setup, find_packages
     from autosys._version import *
 
 _debug_: bool = False
 
-def readme(filename: str = 'README.md')-> str:
+
+def readme(filename: str = "README.md") -> str:
     """ Returns the text of the README file. The default file is `README.md`.
 
     `readme(filename: str = 'README.md')-> str`
@@ -32,10 +33,11 @@ def readme(filename: str = 'README.md')-> str:
     """
     readme_path = Path(__file__).resolve().parents[0] / filename
 
-    with open(readme_path, encoding='utf-8') as f:
+    with open(readme_path, encoding="utf-8") as f:
         return f.read()
 
-class SetupAttrs():
+
+class SetupAttrs:
     """ A wrapper for a dictionary of setup attributes.
 
         Example:
@@ -50,28 +52,30 @@ class SetupAttrs():
         ```
         `Print(s)` will pretty print the attributes.
         """
-#? ---------------------- Setup Attributes
+
+    # ? ---------------------- Setup Attributes
     _dict: Dict = {}
+
     def __init__(self):
         self._dict = {
-            'name' : 'autosys',
-            'version' : __version__,
-            'description' : 'System utilities for Python on macOS',
-            'long_description' : readme(),
-            'long_description_content_type': 'text/markdown',
-            'license' : 'MIT',
-            'author' : 'Michael Treanor',
-            'author_email' : 'skeptycal@gmail.com',
-            'maintainer' : 'Michael Treanor',
-            'maintainer_email' : 'skeptycal@gmail.com',
-            'url' : f"https://skeptycal.github.io/{__title__}/",
-            'python_requires' : '>=3.8',
+            "name": "autosys",
+            "version": __version__,
+            "description": "System utilities for Python on macOS",
+            "long_description": readme(),
+            "long_description_content_type": "text/markdown",
+            "license": "MIT",
+            "author": "Michael Treanor",
+            "author_email": "skeptycal@gmail.com",
+            "maintainer": "Michael Treanor",
+            "maintainer_email": "skeptycal@gmail.com",
+            "url": f"https://skeptycal.github.io/{__title__}/",
+            "python_requires": ">=3.8",
             # 'cmdclass': '',
             # 'command_options': '',
             # 'command_packages': '',
             # 'data_files': '',
             # 'distclass': '',
-            'download_url' : f"https://www.github.com/skeptycal/{__title__}/",
+            "download_url": f"https://www.github.com/skeptycal/{__title__}/",
             # 'ext_modules': '',
             # 'ext_package' : '',
             # 'fullname' : '',
@@ -85,7 +89,7 @@ class SetupAttrs():
             # 'package_dir' : {'': __title__},
             # 'package_dir': '',
             # 'packages': '',
-            'packages' : find_packages(),
+            "packages": find_packages(),
             # 'password' : '',
             # 'platforms': '',
             # 'provides': '',
@@ -94,18 +98,18 @@ class SetupAttrs():
             # 'script_args': '',
             # 'script_name' : '',
             # 'scripts': '',
-            'zip_safe': False,
-            'keywords' : 'application macOS dev cache utilities cli python text console log debug test testing',
-            'package_data' : {
+            "zip_safe": False,
+            "keywords": "application macOS dev cache utilities cli python text console log debug test testing",
+            "package_data": {
                 # If any package contains txt, rst, md files, include them:
                 "": ["*.txt", "*.rst", "*.md", "*.ini", "*.png", "*.jpg"]
             },
-            'project_urls' : {
+            "project_urls": {
                 "Website": f"https://skeptycal.github.io/{__title__}/",
                 "Documentation": f"https://skeptycal.github.io/{__title__}/docs",
                 "Source Code": f"https://www.github.com/skeptycal/{__title__}/",
             },
-            'classifiers': [
+            "classifiers": [
                 "Development Status :: 4 - Beta",
                 "Environment :: Console",
                 "Environment :: MacOS X",
@@ -123,7 +127,8 @@ class SetupAttrs():
                 "Topic :: Utilities",
             ],
         }
-#? ---------------------- SetupAttrs Methods
+
+    # ? ---------------------- SetupAttrs Methods
 
     @property
     def setup(self):
@@ -136,7 +141,8 @@ class SetupAttrs():
         return self.__getitem__(name)
 
     def __str__(self):
-        return pprint.pformat(self._dict,depth=5,width=50,compact=False)
+        return pprint.pformat(self._dict, depth=5, width=50, compact=False)
+
 
 s = SetupAttrs()
 
