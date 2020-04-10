209d7c9cc7e9d4b59f099f2c5e9b45a3f8222cc4
skeptycal <skeptycal@gmail.com>
2020-04-10 10:21:26 -0500

    add tests

--- tests/test_autosys.py
+++ tests/test_autosys.py
@@ -6,10 +6,58 @@ Initial Basic Tests for Autosys package.
 For more information: https://docs.python-guide.org/writing/tests/
 """
 
+import autosys
+from autosys import *
 
-def func(x):
-    return x + 1
+
+def test_version_number():
+    assert isinstance(autosys.__version__, str)
 
 
-def test_answer():
-    assert func(3) == 5
+def test_CONSTANTS():
+    assert autosys.__author__ == "Michael Treanor"
+    assert autosys.__author_email__ == "skeptycal@gmail.com"
+    assert autosys.__copyright__ == "Copyright (c) 2019 Michael Treanor"
+    assert autosys.__license__ == "MIT"
+    assert autosys.__name__ == "autosys"
+    assert autosys.__python_requires__ == ">=3.8"
+    assert autosys.__title__ == "autosys"
+
+
+def test_docopt_import_status():
+    try:
+        import docopt
+
+        assert autosys.DOCOPT_FLAG == True
+    except:
+        assert autosys.DOCOPT_FLAG == False
+
+
+def test_ENV_import():
+    tmp = "SHELL" in autosys.ENV
+    assert tmp == True
+    tmp = "zabiwickerwillowsky" not in autosys.ENV
+    assert tmp == True
+
+
+def test_NL_constant():
+    assert ord(autosys.NL) == 10
+
+
+def print_autosys_dir():
+    DEFAULT_PRINT_WIDTH = 40
+    for i, x in enumerate(dir(autosys)):
+        if i > 30:
+            fmt = f"autosys.{x}"
+            f = str(eval(fmt))
+            if len(f) > DEFAULT_PRINT_WIDTH:
+                f = f[0 : DEFAULT_PRINT_WIDTH - 1]
+            print(f"assert autosys.{x} == '{f}'")
+            print()
+
+
+test_version_number()
+
+
+if __name__ == "__main__":
+    print_autosys_dir()
--- tests/test_www.py
+++ tests/test_www.py
@@ -1,21 +1,24 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-test_www.py
-"""
-# copyright (c) 2019 Michael Treanor
-# https://www.github.com/skeptycal
-# https://www.twitter.com/skeptycal
+# #!/usr/bin/env python3
+# # -*- coding: utf-8 -*-
+# """
+# test_www.py
+# """
+# # copyright (c) 2019 Michael Treanor
+# # https://www.github.com/skeptycal
+# # https://www.twitter.com/skeptycal
 
-from __future__ import absolute_import, print_function
-import pytest
-from pytest import raises
-from autosys.as_www import url_test
+# from __future__ import absolute_import, print_function
+# import pytest
+# from pytest import raises
 
 
-def test_url_test():
-    urls = ['http://www.google.com', 'https://www.google.com',
-            'https://www.twitter.com/skeptycal', 'http://192.168.0.1']
-    # 'https://www.skeptycal.com'
-    for url in urls:
-        assert url_test(url) == 200
+# def test_url_test():
+#     urls = [
+#         "http://www.google.com",
+#         "https://www.google.com",
+#         "https://www.twitter.com/skeptycal",
+#         "http://192.168.0.1",
+#     ]
+#     # 'https://www.skeptycal.com'
+#     for url in urls:
+#         assert url_test(url) == 200
