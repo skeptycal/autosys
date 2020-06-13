7ed55c891105d2247c7470ed53d7920796236a68
skeptycal <skeptycal@gmail.com>
2020-06-13 18:42:33 -0500

    container utilities initial commit

---
+++ autosys/containers/list_diff.py
@@ -0,0 +1,18 @@
+from autosys.text_utils.random_string import *
+from typing import List, Sequence
+
+
+def difference(a: Sequence, b: Sequence) -> (List):
+    """ Return a list of items from `a` that are not in `b`.
+    """
+    return [item for item in set(a) if item not in set(b)]
+
+
+a = [random_string() for i in range(100)]
+b = [random_string() for i in range(100)]
+
+# for i in dir():
+
+# print(i)
+
+print(dir())
