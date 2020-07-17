64c2448a1b71fa08b1a80495b776e43e1b6bc73a
skeptycal <26148512+skeptycal@users.noreply.github.com>
2020-07-16 19:25:29 -0700

    # (GitIt Bot) minor updates and formatting # https://www.github.com/skeptycal

--- conf.py
+++ conf.py
@@ -10,21 +10,15 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-import os
-import sys
-
-# sys.path.insert(0, os.path.abspath('.'))
-sys.path.insert(0, os.path.abspath('./venv/lib/python3.8/site-packages'))
-
 
 # -- Project information -----------------------------------------------------
 
-project = 'AutoSys'
-copyright = '2020, Michael Treanor'
-author = 'Michael Treanor'
+project = "AutoSys"
+copyright = "2020, Michael Treanor"
+author = "Michael Treanor"
 
 # The full version, including alpha/beta/rc tags
-release = '0.4.4'
+release = "0.4.4"
 
 
 # -- General configuration ---------------------------------------------------
@@ -33,20 +27,20 @@ release = '0.4.4'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'sphinx-autobuild',
-    'sphinxcontrib-applehelp',
-    'sphinxcontrib-devhelp',
-    'sphinxcontrib-htmlhelp',
-    'sphinxcontrib-jsmath',
-    'sphinxcontrib-napoleon',
-    'sphinxcontrib-qthelp',
-    'sphinxcontrib-serializinghtml',
+    "sphinx-autobuild",
+    "sphinxcontrib-applehelp",
+    "sphinxcontrib-devhelp",
+    "sphinxcontrib-htmlhelp",
+    "sphinxcontrib-jsmath",
+    "sphinxcontrib-napoleon",
+    "sphinxcontrib-qthelp",
+    "sphinxcontrib-serializinghtml",
 ]
 
 source_suffix = {
-    '.rst': 'restructuredtext',
-    '.txt': 'restructuredtext',
-    '.md': 'markdown',
+    ".rst": "restructuredtext",
+    ".txt": "restructuredtext",
+    ".md": "markdown",
 }
 
 rst_epilog = """
@@ -54,22 +48,22 @@ rst_epilog = """
 """
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+templates_path = ["_templates"]
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = [
-    '**/*bak*',
-    '**/*private*',
-    '_build',
-    '_dist',
-    '**/Thumbs.db',
-    '**/.DS_Store',
-    '.git',
-    '.tox',
-    'bak_ideas',
-    'venv',
+    "**/*bak*",
+    "**/*private*",
+    "_build",
+    "_dist",
+    "**/Thumbs.db",
+    "**/.DS_Store",
+    ".git",
+    ".tox",
+    "bak_ideas",
+    "venv",
 ]
 
 # -- Options for HTML output -------------------------------------------------
@@ -77,9 +71,9 @@ exclude_patterns = [
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = "alabaster"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = ["_static"]
