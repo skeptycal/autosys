8d0880b7b0f21b1d360ae7ed08dbdcee5c79ff7d
skeptycal <26148512+skeptycal@users.noreply.github.com>
2020-08-02 17:37:36 -0500

    pytest additions and config

--- tests/test_pytest.py
+++ tests/test_pytest.py
@@ -1,4 +1,7 @@
-def _add(a: int, b: int) -> (int):
+""" A few simple tests used to check pytest and coverage configuration. """
+
+
+def __add(a: int, b: int) -> (int):
     return a + b
 
 
@@ -6,11 +9,18 @@ def test_add():
     for i in range(100):
         a = 3 * i
         b = 4 * i
-        assert a + b == _add(a, b)
+        assert a + b == __add(a, b)
 
 
-def test_new():
+def test_true():
     """
     Test Doc: Line 1 ...
     """
     assert True
+
+
+def test_false():
+    """
+    Test Doc: Line 1 ...
+    """
+    assert False
