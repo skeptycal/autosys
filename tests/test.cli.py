4ba07b243958d7eed2f9f447e2ef88ee75ed5246
skeptycal <26148512+skeptycal@users.noreply.github.com>
2020-08-26 20:45:57 -0500

    # (GitIt Bot) minor updates and formatting # https://www.github.com/skeptycal

---
+++ tests/test.cli.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: UTF-8 -*-
+"""
+Initial Basic Tests for Autosys package.
+For more information: https://docs.python-guide.org/writing/tests/
+"""
+from autosys.cli import *
+
+
+def test_stdout():
+    assert stdout.hasattr(tty)
+
+
+if __name__ == '__main__':
+    pass
+    # print_autosys_dir(globals())
---
+++ tests/test_debug.py
@@ -0,0 +1,33 @@
+# #!/usr/bin/env python3
+# # -*- coding: UTF-8 -*-
+
+# """
+# Tests for Autosys package. For more information:
+# https://docs.python-guide.org/writing/tests/
+# """
+
+# # import autosys
+
+
+# def logical_xor(a, b):
+#     return (a and not b) or (not a and b)
+
+
+# test_data = [
+#     [False, False, ],
+#     [False, True],
+#     [True, False],
+#     [True, True],
+#     ['one', 'one'],
+#     ['one', 'two'],
+#     ['False', 'False'],
+#     ['False', 'True'],
+#     [1, 1],
+#     [1, 2],
+#     [0, 0],
+#     [0, 1],
+#     [None, None],
+#     ['1', 1]
+# ]
+
+# args = test_data
